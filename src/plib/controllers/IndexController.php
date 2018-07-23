4c323e14bfc371f66d611fd6fa45aedfc69f06e7
Lauris Binde <lauris@lauris.lv>
2018-07-23 13:53:41 +0200

    Add Composer support and namespacing

--- src/plib/controllers/IndexController.php
+++ src/plib/controllers/IndexController.php
@@ -1,7 +1,9 @@
 <?php
 // Copyright 1999-2018. Plesk International GmbH. All rights reserved.
 
-class IndexController extends pm_Controller_Action
+use PleskExt\DiskspaceUsageViewer\Helper;
+
+class IndexController extends \pm_Controller_Action
 {
     private $currentPath = '/';
 
@@ -11,7 +13,7 @@ class IndexController extends pm_Controller_Action
     {
         parent::init();
 
-        $this->view->headLink()->appendStylesheet(pm_Context::getBaseUrl() . 'styles.css');
+        $this->view->headLink()->appendStylesheet(\pm_Context::getBaseUrl() . 'styles.css');
 
         $this->setPath('/');
     }
@@ -39,16 +41,16 @@ class IndexController extends pm_Controller_Action
     private function setPath($path)
     {
         if (empty($path)) {
-            throw new Exception('Path must not be null');
+            throw new \Exception('Path must not be null');
         }
 
         // add security handling to ensure only paths can be viewed with current priviledges
         // add security handling to prevent manipulating path string
-        if (pm_Session::getClient()->isAdmin()) {
+        if (\pm_Session::getClient()->isAdmin()) {
             $this->currentPath = $path;
         } else {
             // TODO: get path of webspace of current user and make sure this is always the base directory
-            $domains = pm_Domain::getDomainsByClient(pm_Session::getClient(), true);
+            $domains = \pm_Domain::getDomainsByClient(\pm_Session::getClient(), true);
 
             if (sizeof($domains) > 0) {
                 var_dump('getVhostSystemPath=' . $domains[0]->getVhostSystemPath());
@@ -56,7 +58,7 @@ class IndexController extends pm_Controller_Action
                 var_dump('getHomePath=' . $domains[0]->getHomePath());
                 $this->currentPath = $domains[0]->getVhostSystemPath();
             } else {
-                throw new Exception('No webspace found for this user!');
+                throw new \Exception('No webspace found for this user!');
             }
         }
 
@@ -81,7 +83,7 @@ class IndexController extends pm_Controller_Action
         foreach ($folders as $folder) {
             if (!empty($folder)) {
                 $path .= $folder . '/';
-                $breadCrumb .= '<a href="' . pm_Context::getBaseUrl() . '?path=' . $path .'">' . $folder . '</a>/';
+                $breadCrumb .= '<a href="' . \pm_Context::getBaseUrl() . '?path=' . $path .'">' . $folder . '</a>/';
             }
         }
 
@@ -96,35 +98,34 @@ class IndexController extends pm_Controller_Action
         if ($path != '/') {
             $data[] = [
                 'size' => '<span class="hidden">9999999999</span>',
-                'folder' => '<a href="' . pm_Context::getBaseUrl() . '?path=' . Modules_DiskspaceUsageViewer_DiskspaceUsageViewer::getParentPath($this->currentPath) .'">..</a>',
+                'folder' => '<a href="' . \pm_Context::getBaseUrl() . '?path=' . Helper::getParentPath($this->currentPath) .'">..</a>',
             ];
         }
 
-        $diskspaceUsageViewer = new Modules_DiskspaceUsageViewer_DiskspaceUsageViewer();
-        $folders = $diskspaceUsageViewer->getDiskspaceUsage($path);
+        $folders = Helper::getDiskspaceUsage($path);
 
         foreach ($folders as $folder) {
             $data[] = [
-                'size' => '<span class="hidden">' . str_pad($folder[0], 10, '0', STR_PAD_LEFT) . '</span>' . Modules_DiskspaceUsageViewer_DiskspaceUsageViewer::formatSize($folder[0]),
-                'folder' => '<a href="' . pm_Context::getBaseUrl() . '?path=' . $this->getFullPath($folder[1]) .'">' . $folder[1] . '</a>',
+                'size' => '<span class="hidden">' . str_pad($folder[0], 10, '0', STR_PAD_LEFT) . '</span>' . Helper::formatSize($folder[0]),
+                'folder' => '<a href="' . \pm_Context::getBaseUrl() . '?path=' . $this->getFullPath($folder[1]) .'">' . $folder[1] . '</a>',
             ];
         }
 
         $options = array (
             'defaultSortField' => 'size',
-            'defaultSortDirection' => pm_View_List_Simple::SORT_DIR_DOWN,
+            'defaultSortDirection' => \pm_View_List_Simple::SORT_DIR_DOWN,
         );
 
-        $list = new pm_View_List_Simple($this->view, $this->_request, $options);
+        $list = new \pm_View_List_Simple($this->view, $this->_request, $options);
 
         $list->setColumns([
             'size' => [
-                'title' => pm_Locale::lmsg('columnSize'),
+                'title' => \pm_Locale::lmsg('columnSize'),
                 'noEscape' => true,
                 'sortable' => true,
             ],
             'folder' => [
-                'title' => pm_Locale::lmsg('columnFolder'),
+                'title' => \pm_Locale::lmsg('columnFolder'),
                 'noEscape' => true,
                 'sortable' => true,
                 'searchable' => true,
