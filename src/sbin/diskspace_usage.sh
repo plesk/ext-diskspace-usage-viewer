1c39b816525704de543544cbb7bc59da287dc688
Lauris Binde <lauris@lauris.lv>
2018-07-24 12:02:39 +0200

    Remove unused 2nd param

--- src/sbin/diskspace_usage.sh
+++ src/sbin/diskspace_usage.sh
@@ -6,8 +6,4 @@ else
     cd /
 fi
 
-if [[ -n $2 ]]; then
-    du -d 1 | sed 's#.\/##g' | sort -n -r | head -n $2
-else
-    du -d 1 | sed 's#.\/##g' | sort -n -r
-fi
+du -d 1 | sed 's#.\/##g' | sort -n -r
