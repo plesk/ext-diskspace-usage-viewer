a9f7c190d0c392d27ec9c5e18f169665160eabb1
Viktor Vogel <viktor@plesk.com>
2018-12-07 14:59:55 +0100

    Added Plesk UI library - WIP

---
+++ frontend/containers/Home/Home.js
@@ -0,0 +1,13 @@
+/* eslint-disable react/jsx-max-depth */
+
+import {createElement, Fragment} from '@plesk/ui-library';
+
+const Home = ({...props}) => (
+    <Fragment>
+        <div id="diskspace-usage-viewer">
+            /* TODO: Add content */
+        </div>
+    </Fragment>
+);
+
+export default Home;
