1822a21e4b88fb1e90d0a7b29b9a95d58bd692cb
Lauris Binde <lauris@lauris.lv>
2019-01-08 14:21:13 +0100

    Top biggest files

--- src/plib/library/Db.php
+++ src/plib/library/Db.php
@@ -53,4 +53,44 @@ class Db
 
         return (int) $row['size'];
     }
+
+    public static function saveFiles(array $files): void
+    {
+        self::adapter()->exec('DELETE FROM `files`');
+
+        $stmt = self::adapter()->prepare('INSERT INTO `files` (`path`, `size`) VALUES (:path, :size)');
+
+        foreach ($files as $file) {
+            $stmt->execute([
+                'path' => $file['path'],
+                'size' => $file['size'],
+            ]);
+        }
+    }
+
+    public static function getFiles(): array
+    {
+        $sql = <<<SQL
+SELECT *
+FROM `files`
+ORDER BY `size` DESC
+SQL;
+
+        $files = [];
+
+        foreach (Db::adapter()->fetchAssoc($sql) as $row) {
+            $files[$row['id']] = $row;
+        }
+
+        return $files;
+    }
+
+    public static function deleteFileById(int $id): void
+    {
+        $stmt = self::adapter()->prepare('DELETE FROM `files` WHERE `id` = :id');
+
+        $stmt->execute([
+            'id' => $id,
+        ]);
+    }
 }
